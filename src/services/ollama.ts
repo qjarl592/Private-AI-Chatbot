f275267e9f650af145c7f86d4ae87a0c1b3651a6
John <john10.kim@cheilpengtai.com>
2025-06-09 16:04:09 +0900

    feat: zod 스키마 검정 추가

--- src/services/ollama.ts
+++ src/services/ollama.ts
@@ -1,9 +1,36 @@
 import axios from "axios";
+import { z } from "zod";
 
 const ollamaApiInstance = axios.create({
   baseURL: "http://localhost:11434",
 });
 
-export function getStatus() {
-  return ollamaApiInstance.get("/api/tags");
+const OllamaModelDetailSchema = z.object({
+  parent_model: z.string(),
+  format: z.string(),
+  family: z.string(),
+  families: z.array(z.string()),
+  parameter_size: z.string(),
+  quantization_level: z.string(),
+});
+export type OllamaModelDetail = z.infer<typeof OllamaModelDetailSchema>;
+
+const OllamaModelSchema = z.object({
+  name: z.string(),
+  model: z.string(),
+  modified_at: z.string(),
+  size: z.number(),
+  digest: z.string(),
+  details: OllamaModelDetailSchema,
+});
+export type OllamaModel = z.infer<typeof OllamaModelSchema>;
+
+const OllamaModelListSchema = z.object({
+  models: z.array(OllamaModelSchema),
+});
+export type OllamaModelList = z.infer<typeof OllamaModelListSchema>;
+
+export async function getStatus() {
+  const { data } = await ollamaApiInstance.get("/api/tags");
+  return OllamaModelListSchema.parse(data);
 }
