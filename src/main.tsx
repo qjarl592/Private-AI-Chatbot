36e20519ad3b2030f57ad8125ec113e8a8e845da
John <john10.kim@cheilpengtai.com>
2025-06-25 13:44:07 +0900

    feat: shadcn sonner 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import QueryProvider from "./components/provider/QueryProvider.tsx";
 import RouteProvider from "./components/provider/RouteProvider.tsx";
+import { Toaster } from "./components/shadcn/sonner.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Toaster />
     <QueryProvider>
       <RouteProvider />
     </QueryProvider>
