ce0900b2452e4977734ec89ce594af18467d6548
John <john10.kim@cheilpengtai.com>
2025-06-25 11:10:43 +0900

    feat: route redirect 설정

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,14 +1,19 @@
 import Guide from "@/pages/guide/Guide";
 import Chat from "@/pages/chat/Chat";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import MainLayout from "@/components/layout/MainLayout";
 import NewChat from "@/pages/chat/NewChat";
 
 export const router = createBrowserRouter([
   {
+    path: "/",
+    element: <Navigate to="/chat" replace />,
+  },
+  {
     path: "chat",
     element: <MainLayout />,
     children: [
+      { index: true, element: <Navigate to="/chat/new" replace /> },
       { path: "new", element: <NewChat /> },
       { path: ":chatId", element: <Chat /> },
     ],
